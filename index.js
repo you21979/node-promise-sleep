e76fae3a2a6666aafb1bcf68837334917a810d74
you21979 <you2197901@gmail.com>
2016-10-19 16:21:57 +0900

    mod dir

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 'use strict';
-
-var Promise = require('bluebird');
-
-var sleep = module.exports = function(msec, value){
-    return new Promise(function(resolve){
-        setTimeout(function(){
-            resolve(value);
-        }, msec)
-    })
-}
-
+module.exports = require('./lib/sleep');
