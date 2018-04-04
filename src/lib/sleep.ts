d1d86554fe584b5f823b23afde190cc8a6853fdb
you21979 <you2197901@gmail.com>
2018-04-04 19:32:53 +0900

    fix option param

--- src/lib/sleep.ts
+++ src/lib/sleep.ts
@@ -1,6 +1,6 @@
 type typeofTimeoutAlias = (callback : () => void, msec : number) => void
 
-export const sleep = (msec : number, value : any) : Promise<any> => {
+export const sleep = (msec : number, value : any = void 0) : Promise<any> => {
     const f : typeofTimeoutAlias = (msec > 0) ? setTimeout : setImmediate;
     return new Promise((resolve : (any) => void) => {
         f(() => {
